25a7019f895cebf02709797a49f99e6405daf56e
ivanmo20 <63992740+ivanmo20@users.noreply.github.com>
2020-05-18 19:53:00 -0500

    user2

--- Laboratorio5-SW2/src/main/java/sw2/lab5/repository/UserRepository.java
+++ Laboratorio5-SW2/src/main/java/sw2/lab5/repository/UserRepository.java
@@ -1,7 +1,8 @@
 package sw2.lab5.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository {
+public interface UserRepository extends JpaRepository<User,Integer> {
 }
